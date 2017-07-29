711af4a6439e90474f835f1a8cb9dbbea8d69315
leiwenchang <494391740@qq.com>
2017-07-29 17:32:25 +0800

    add spark context

--- src/main/scala/leiwenchang/hello.scala
+++ src/main/scala/leiwenchang/hello.scala
@@ -1,10 +1,16 @@
 package leiwenchang
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Administrator on 2017/7/29.
   */
 object hello {
   def main(args: Array[String]) {
+    val sparkconf = new SparkConf().setMaster("local").setAppName("hello")
+    val sc = new SparkContext(sparkconf)
+    val rdd=sc.parallelize(Array(1,2,3))
+    println(rdd.count())
     println("hello")
   }
 }
